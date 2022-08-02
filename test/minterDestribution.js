9ac3c5c6b5aceaf3154c8f5338b1741c431a85aa
RuslanMirov <ruslan777mirov@gmail.com>
2022-08-02 11:11:36 +0300

    update tests

--- test/minterDestribution.js
+++ test/minterDestribution.js
@@ -18,7 +18,8 @@ function getCreate2Address(
   return getAddress(`0x${keccak256(sanitizedInputs).slice(-40)}`)
 }
 
-describe("supply", function () {
+describe("Minter destribution", function () {
+  const teamLockPeriod = 10368000
 
   let token;
   let ve_underlying;
@@ -26,26 +27,28 @@ describe("supply", function () {
   let owner;
   let minter;
   let ve_dist;
-  let fetch;
   let wftm;
   let ethPair;
   let ethPairToken;
   let treasury;
   let gauge_address;
   let destributor;
+  let teamWallet;
 
-  const ve_initial = ethers.BigNumber.from("1000000000000000000000");
-
-  it("deploy base", async function () {
+  it("Minter destribution deploy", async function () {
     [owner] = await ethers.getSigners(1);
     token = await ethers.getContractFactory("Token");
     basev1 = await ethers.getContractFactory("BaseV1");
+
+    // SKIPPED
     mim = await token.deploy('MIM', 'MIM', 18, owner.address);
     await mim.mint(owner.address, ethers.BigNumber.from("1000000000000000000000000000000"));
+    // SKIPPED
+
     ve_underlying = await basev1.deploy();
     vecontract = await ethers.getContractFactory("contracts/ve.sol:ve");
     ve = await vecontract.deploy(ve_underlying.address);
-    await ve_underlying.mint(owner.address, ve_initial);
+    await ve_underlying.mint(owner.address, ethers.BigNumber.from("10000000000000000000000000"));
     const BaseV1Factory = await ethers.getContractFactory("BaseV1Factory");
     factory = await BaseV1Factory.deploy();
     await factory.deployed();
@@ -62,23 +65,24 @@ describe("supply", function () {
     const bribe_factory = await BaseV1BribeFactory.deploy();
     await bribe_factory.deployed();
     const BaseV1Voter = await ethers.getContractFactory("BaseV1Voter");
-    const gauge_factory = await BaseV1Voter.deploy(ve.address, factory.address, gauges_factory.address, bribe_factory.address);
-    await gauge_factory.deployed();
+    const voter_gauge_factory = await BaseV1Voter.deploy(ve.address, factory.address, gauges_factory.address, bribe_factory.address);
+    await voter_gauge_factory.deployed();
 
-    await gauge_factory.initialize([mim.address, ve_underlying.address],owner.address);
+    await voter_gauge_factory.initialize([mim.address, ve_underlying.address],owner.address);
     await ve_underlying.approve(ve.address, ethers.BigNumber.from("1000000000000000000"));
     await ve.create_lock(ethers.BigNumber.from("1000000000000000000"), 4 * 365 * 86400);
+
     const VeDist = await ethers.getContractFactory("contracts/ve_dist.sol:ve_dist");
     ve_dist = await VeDist.deploy(ve.address);
     await ve_dist.deployed();
-    await ve.setVoter(gauge_factory.address);
+    await ve.setVoter(voter_gauge_factory.address);
 
     const BaseV1Minter = await ethers.getContractFactory("BaseV1Minter");
     minter = await BaseV1Minter.deploy(
-      gauge_factory.address,
+      voter_gauge_factory.address,
       ve.address,
       ve_dist.address,
-      10368000 // 120 days in seconds
+      teamLockPeriod // 120 days in seconds
     );
     await minter.deployed();
     await ve_dist.setDepositor(minter.address);
@@ -92,36 +96,40 @@ describe("supply", function () {
 
     const pair = await router.pairFor(mim.address, ve_underlying.address, false);
 
-    await ve_underlying.approve(gauge_factory.address, ethers.BigNumber.from("500000000000000000000000"));
-    await gauge_factory.createGauge(pair);
+    await ve_underlying.approve(voter_gauge_factory.address, ethers.BigNumber.from("500000000000000000000000"));
+    await voter_gauge_factory.createGauge(pair);
     expect(await ve.balanceOfNFT(1)).to.above(ethers.BigNumber.from("995063075414519385"));
     expect(await ve_underlying.balanceOf(ve.address)).to.be.equal(ethers.BigNumber.from("1000000000000000000"));
 
-    await gauge_factory.vote(1, [pair], [5000]);
+    await voter_gauge_factory.vote(1, [pair], [5000]);
+    gauge_address = await voter_gauge_factory.gauges(pair);
+
 
     const Treasury = await ethers.getContractFactory("Treasury");
-    treasury = await Treasury.deploy();
-    await treasury.deployed();
+    treasury = await Treasury.deploy()
+    await treasury.deployed()
 
-    const FetchFormula = await ethers.getContractFactory("FetchFormulaMock");
-    fetch_formula = await FetchFormula.deploy();
-    await fetch_formula.deployed();
+    const TeamWallet = await ethers.getContractFactory("TeamWallet");
+    teamWallet = await TeamWallet.deploy(ve_underlying.address);
+    await teamWallet.deployed();
 
-    const Fetch = await ethers.getContractFactory("Fetch");
 
-    fetch = await Fetch.deploy(
-      router.address,
-      ve_underlying.address,
-      owner.address,
-      minter.address,
-      ve.address,
-      treasury.address,
-      fetch_formula.address
+    const RewardsLocker = await ethers.getContractFactory("VotersRewardsLock");
+    rewardsLocker = await RewardsLocker.deploy(
+      voter_gauge_factory.address,
+      ve_underlying.address
     );
+    rewardsLocker.deployed();
 
-    gauge_address = await gauge_factory.gauges(pair);
+    const RewardsFormula = await ethers.getContractFactory("VotersRewardsFormula");
+    rewardsFormula = await RewardsFormula.deploy(
+      ve.address,
+      rewardsLocker.address,
+      ve_underlying.address
+    );
+    rewardsFormula.deployed();
 
-    await fetch.deployed();
+    rewardsLocker.updateFormula(rewardsFormula.address)
 
     GaugesRewardDestributor = await ethers.getContractFactory("GaugesRewardDestributor");
 
@@ -133,66 +141,49 @@ describe("supply", function () {
     destributor.deployed();
 
     await minter.initialize(
-      fetch.address,
-      0,
+      owner.address,  // should be fetch
+      ethers.BigNumber.from("10000000000000000000"),
       destributor.address,
-      owner.address, // should be vote locker
-      owner.address // should be team wallet
+      rewardsLocker.address,
+      teamWallet.address
     );
 
-    expect(await fetch.dexRouter()).to.equal(router.address);
-
-    const tokenLD = await ve_underlying.balanceOf(owner.address)
-    const ethLD = "1000000000000000000"
-
-    // await network.provider.send("evm_mine")
-
-    await ve_underlying.approve(router.address, tokenLD)
-
-    await wftm.deposit({value:"1000000000000000000"})
-
-    await router.addLiquidityFTM(
-      ve_underlying.address,
-      false,
-      tokenLD,
-      1,
-      1,
-      owner.address,
-      Date.now(),
-      { value:ethLD }
-    )
-
-    ethPair = await router.pairFor(
-      wftm.address,
-      ve_underlying.address,
-      false
-    );
-
-    ethPairToken = await token.attach(
-      ethPair
-    );
   });
 
-  it("Test supply", async function () {
+  it("First destribution team wallet should not receive, 0x should", async function () {
+    expect(await ve_underlying.balanceOf(destributor.address)).to.be.equal(0);
+    expect(await ve_underlying.balanceOf(rewardsLocker.address)).to.be.equal(0);
+    expect(await ve_underlying.balanceOf(teamWallet.address)).to.be.equal(0);
+    expect(await ve_underlying.balanceOf("0x0000000000000000000000000000000000000000")).to.be.equal(0);
 
-    const week = 604800
+    await network.provider.send("evm_increaseTime", [86400 * 21])
 
-    console.log("Supply before tests", Web3Utils.fromWei(String(await ve_underlying.totalSupply())))
+    await minter.update_period()
 
-    for(let i = 0; i < 7; i++){
-      console.log(`Week ${i + 1}`,"_______________________________________________________________________________________")
-      console.log(`Supply before`, Number(Web3Utils.fromWei(String(await ve_underlying.totalSupply()))))
-      // increase time
-      await network.provider.send("evm_increaseTime", [week])
-      await network.provider.send("evm_mine")
-      // console.log(`Weekly emmision`, Number(Web3Utils.fromWei(String(await minter.weekly_emission()))))
-      // trigger minter
-      await minter.update_period()
-      console.log(`Supply after`, Number(Web3Utils.fromWei(String(await ve_underlying.totalSupply()))))
-      console.log("_______________________________________________________________________________________")
-    }
+    expect(await ve_underlying.balanceOf(destributor.address)).to.not.equal(0);
+    expect(await ve_underlying.balanceOf(rewardsLocker.address)).to.not.equal(0);
+    expect(await ve_underlying.balanceOf("0x0000000000000000000000000000000000000000")).to.not.equal(0);
 
-    console.log("Supply after tests", Web3Utils.fromWei(String(await ve_underlying.totalSupply())))
+    // team wallet should not receive any tokens yet
+    expect(await ve_underlying.balanceOf(teamWallet.address)).to.be.equal(0);
   });
 
+
+  it("Second destribution after increase time team wallet should receive, 0x should not", async function () {
+
+    const zerroBalanceBefore = await ve_underlying.balanceOf("0x0000000000000000000000000000000000000000")
+
+    await network.provider.send("evm_increaseTime", [teamLockPeriod])
+
+    await minter.update_period()
+
+    expect(await ve_underlying.balanceOf(destributor.address)).to.not.equal(0);
+    expect(await ve_underlying.balanceOf(rewardsLocker.address)).to.not.equal(0);
+
+    // zerro should not
+    expect(await ve_underlying.balanceOf("0x0000000000000000000000000000000000000000")).to.be.equal(zerroBalanceBefore);
+
+    // team wallet should receive now
+    expect(await ve_underlying.balanceOf(teamWallet.address)).to.not.equal(0);
+  });
 });
