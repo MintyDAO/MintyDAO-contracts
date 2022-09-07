6c59d6f6e449294b567b52b0f287c5e64f7f92e0
RuslanMirov <ruslan777mirov@gmail.com>
2022-09-07 12:37:01 +0300

    Updtae tests

---
+++ test/gauge-wl.js
@@ -0,0 +1,131 @@
+const { expect } = require("chai");
+const { ethers, waffle } = require("hardhat");
+const provider = waffle.provider;
+const Web3Utils = require('web3-utils');
+
+const minLockTime = 10368000 // 120 days in seconds
+
+function getCreate2Address(
+  factoryAddress,
+  [tokenA, tokenB],
+  bytecode
+) {
+  const [token0, token1] = tokenA < tokenB ? [tokenA, tokenB] : [tokenB, tokenA]
+  const create2Inputs = [
+    '0xff',
+    factoryAddress,
+    keccak256(solidityPack(['address', 'address'], [token0, token1])),
+    keccak256(bytecode)
+  ]
+  const sanitizedInputs = `0x${create2Inputs.map(i => i.slice(2)).join('')}`
+  return getAddress(`0x${keccak256(sanitizedInputs).slice(-40)}`)
+}
+
+describe("fetch", function () {
+
+  let token;
+  let ve_underlying;
+  let ve;
+  let owner;
+  let minter;
+  let ve_dist;
+  let fetch_formula;
+  let fetch;
+  let wftm;
+  let ethPair;
+  let ethPairToken;
+  let treasury;
+  let gauge_address;
+  let destributor;
+  let operWallet;
+  let voter_gauge_factory;
+  let gaugeWL;
+
+  it("deploy base gauge", async function () {
+    [owner, secondAccount] = await ethers.getSigners(1);
+    token = await ethers.getContractFactory("Token");
+    basev1 = await ethers.getContractFactory("BaseV1");
+
+    // SKIPPED
+    mim = await token.deploy('MIM', 'MIM', 18, owner.address);
+    await mim.mint(owner.address, ethers.BigNumber.from("1000000000000000000000000000000"));
+    // SKIPPED
+
+    ve_underlying = await basev1.deploy();
+    vecontract = await ethers.getContractFactory("contracts/ve.sol:ve");
+    ve = await vecontract.deploy(ve_underlying.address);
+    await ve_underlying.mint(owner.address, ethers.BigNumber.from("10000000000000000000000000"));
+    const BaseV1Factory = await ethers.getContractFactory("BaseV1Factory");
+    factory = await BaseV1Factory.deploy();
+    await factory.deployed();
+    const WETH9 = await ethers.getContractFactory("WETH9");
+    wftm = await WETH9.deploy()
+    await wftm.deployed()
+    const BaseV1Router = await ethers.getContractFactory("BaseV1Router01");
+    router = await BaseV1Router.deploy(factory.address, wftm.address);
+    await router.deployed();
+    const BaseV1GaugeFactory = await ethers.getContractFactory("BaseV1GaugeFactory");
+    gauges_factory = await BaseV1GaugeFactory.deploy();
+    await gauges_factory.deployed();
+    const BaseV1BribeFactory = await ethers.getContractFactory("BaseV1BribeFactory");
+    const bribe_factory = await BaseV1BribeFactory.deploy();
+    await bribe_factory.deployed();
+
+    const GaugeWL = await ethers.getContractFactory("GaugeWhiteList");
+    gaugeWL = await GaugeWL.deploy();
+    await gaugeWL.deployed();
+
+    const BaseV1Voter = await ethers.getContractFactory("BaseV1Voter");
+    voter_gauge_factory = await BaseV1Voter.deploy(
+      ve.address,
+      factory.address,
+      gauges_factory.address,
+      bribe_factory.address,
+      gaugeWL.address
+    );
+    await voter_gauge_factory.deployed();
+
+    // SKIPPED
+    await voter_gauge_factory.initialize([mim.address, ve_underlying.address],owner.address);
+    await ve_underlying.approve(ve.address, ethers.BigNumber.from("1000000000000000000"));
+    await ve.create_lock(ethers.BigNumber.from("1000000000000000000"), 4 * 365 * 86400);
+    // SKIPPED
+
+    const VeDist = await ethers.getContractFactory("contracts/ve_dist.sol:ve_dist");
+    ve_dist = await VeDist.deploy(ve.address);
+    await ve_dist.deployed();
+    await ve.setVoter(voter_gauge_factory.address);
+
+    const BaseV1Minter = await ethers.getContractFactory("BaseV1Minter");
+    minter = await BaseV1Minter.deploy(
+      voter_gauge_factory.address,
+      ve.address,
+      ve_dist.address,
+      10368000 // 120 days in seconds
+    );
+    await minter.deployed();
+    await ve_dist.setDepositor(minter.address);
+    await ve_underlying.setMinter(minter.address);
+
+    // SKIPPED
+    const mim_1 = ethers.BigNumber.from("1000000000000000000");
+    const ve_underlying_1 = ethers.BigNumber.from("1000000000000000000");
+    await ve_underlying.approve(router.address, ve_underlying_1);
+    await mim.approve(router.address, mim_1);
+    await router.addLiquidity(mim.address, ve_underlying.address, false, mim_1, ve_underlying_1, 0, 0, owner.address, Date.now());
+  });
+
+  it("can not create gauge if one of tokens not in WL", async function () {
+    const pair = await router.pairFor(mim.address, ve_underlying.address, false);
+    await ve_underlying.approve(voter_gauge_factory.address, ethers.BigNumber.from("500000000000000000000000"));
+    await expect(voter_gauge_factory.createGauge(pair))
+       .to.be.revertedWith('!Gauge WhiteListed');
+  })
+
+  it("can create gauge if one of tokens in WL", async function () {
+    const pair = await router.pairFor(mim.address, ve_underlying.address, false);
+    await gaugeWL.addToken(ve_underlying.address)
+    await voter_gauge_factory.createGauge(pair)
+  })
+
+});
